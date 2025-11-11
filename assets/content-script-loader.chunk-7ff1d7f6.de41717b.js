c9acb7b6fcbca90ec31805c4cb71f689607ea389
Quốc Tài <nguyendinhquoctai@gmail.com>
2025-11-11 15:20:34 +0700

    Add files via upload

---
+++ assets/content-script-loader.chunk-7ff1d7f6.de41717b.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  (async () => {
+    await import(
+      /* @vite-ignore */
+      chrome.runtime.getURL("assets/chunk-7ff1d7f6.js")
+    );
+  })().catch(console.error);
+
+})();
